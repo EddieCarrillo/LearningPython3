2e9302ca9ca1940f1f9d97ca79096a64d8f95bec
Eduardo Carrillo <edcarril@ucsd.edu>
2026-03-17 10:19:25 -0700

    Create containsduplicate.py

---
+++ leetcode/containsduplicate.py
@@ -0,0 +1,10 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        map = {}
+        for num in nums:
+            if num not in map:
+                map[num] = 1
+            else:
+                return True
+        return False
+        
